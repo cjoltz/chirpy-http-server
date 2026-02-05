9d5febd2d968326553ae12c2e44851d20b1116f7
Christian Colon <cjcolon723@gmail.com>
2026-02-04 20:18:52 -0400

    Added db with users table

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/cjoltz/chirpy-http-server
 
 go 1.25.6
+
+require github.com/google/uuid v1.6.0
+
+require github.com/lib/pq v1.11.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/cjoltz/chirpy-http-server/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
-	apiCfg := apiConfig{}
+	// Load Env Variables
+	godotenv.Load()
+	// Connect to DB
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening to database: %v\n", err)
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
+	}
 
 	mux := http.NewServeMux()
 	// App
@@ -31,6 +53,7 @@ func main() {
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
